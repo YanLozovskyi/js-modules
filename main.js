faf9ae26a775db30e02ccb2b49facd942a0b33aa
YanLozovskyi <yanlozovskiy@gmail.com>
2023-03-14 13:41:45 +0200

    .

---
+++ main.js
@@ -0,0 +1,24 @@
+
+const min = 5;
+const max = 15;
+let total = 0;
+
+for (let i = min; i <= max; i = +1) {
+  console.log(i);
+
+  if (i % 2 === 0) {
+    console.log(`Чётное: `, i);
+  }
+}
+
+result =
+  message.toLowerCase().includes("spam") ||
+  message.toLowerCase().includes("sale");
+
+  console.log(checkForSpam("Latest technology news"));
+  console.log(checkForSpam("JavaScript weekly newsletter"));
+  console.log(checkForSpam("Get best sale offers now!"));
+  console.log(checkForSpam("Amazing SalE, only tonight!"));
+  console.log(checkForSpam("Trust me, this is not a spam message"));
+  console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+  console.log(checkForSpam("[SPAM] How to earn fast money?"));
